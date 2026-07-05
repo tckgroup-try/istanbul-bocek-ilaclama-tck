6edb9cfb31606b51114bf3189c11942528b2bec6
Sovereign Elite <sovereign@godmode.com>
2026-07-06 02:46:06 +0300

    fix(seo): support NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION env variable in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+  },
 };
 
 export default function RootLayout({
